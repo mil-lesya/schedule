4c98e2974ca408e05b28eb4c4533b869dcbfdf1a
ivanjermakov <vania6600@gmail.com>
2019-04-19 18:08:58 +0300

    reserved sql entity name fix

--- core/src/main/java/com/gmail/mileshko/lesya/schedule/entity/Group.java
+++ core/src/main/java/com/gmail/mileshko/lesya/schedule/entity/Group.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "group")
+@Table(name = "\"group\"")
 public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
