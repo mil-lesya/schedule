4e5a790c256000a77319df4a39900c6317ba0cca
ivanjermakov <vania6600@gmail.com>
2019-05-13 21:37:34 +0300

    fix dto recursion

--- core/src/main/java/com/gmail/mileshko/lesya/schedule/dto/ScheduleDto.java
+++ core/src/main/java/com/gmail/mileshko/lesya/schedule/dto/ScheduleDto.java
@@ -11,18 +11,16 @@ public class ScheduleDto {
     public Integer classNumber;
     public Integer periodicity;
     public Week week;
-    public List<GroupDto> groupList;
 
     public ScheduleDto() {
     }
 
-    public ScheduleDto(SubjectDto subjectDto, AuditoryDto auditoryDto, LecturerDto lecturerDto, Integer classNumber, Integer periodicity, Week week, List<GroupDto> groupList) {
+    public ScheduleDto(SubjectDto subjectDto, AuditoryDto auditoryDto, LecturerDto lecturerDto, Integer classNumber, Integer periodicity, Week week) {
         this.subjectDto = subjectDto;
         this.auditoryDto = auditoryDto;
         this.lecturerDto = lecturerDto;
         this.classNumber = classNumber;
         this.periodicity = periodicity;
         this.week = week;
-        this.groupList = groupList;
     }
 }
